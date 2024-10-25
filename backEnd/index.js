90aadb7146b4a340b0cb682a81e1bb5cbe1fb445
Francis <frluemdemu@gmail.com>
2024-10-25 17:26:12 +0800

    modified index

--- backEnd/index.js
+++ backEnd/index.js
@@ -5,3 +5,15 @@ import pkg from 'pg';
 import dotenv from 'dotenv';
 
 const { Pool } = pkg;
+
+const express = require('express')
+const app = express()
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
